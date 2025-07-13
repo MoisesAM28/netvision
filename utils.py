3203224557ccbc5706baa59216e364a8685f03dd
MoisesAM28 <moises.acosta1828@gmail.com>
2025-07-13 15:42:49 -0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ def listar_interfaces():
         for linea in lineas:
             # Ejemplo línea: 1. \Device\NPF_{...} (Conexión de área local* 12)
             partes = linea.split(". ", 1)[1]  # toma después del número y punto
-            # Quedarse solo con la parte antes del primer espacio (el nombre sin descripción)
+            
             nombre = partes.split(" ")[0]
             interfaces.append(nombre)
         return interfaces
